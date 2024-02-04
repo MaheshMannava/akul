c2e5337606df0e8cd850176619357473f7c4079e
MaheshMannava <maheshmannava16@gmail.com>
2024-02-05 02:25:40 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./app.scss"
+import Main from "./components/main/Main";
+
+const App = () => {
+  return <div>
+    <section>
+      <Main />
+    </section>
+  </div>;
+};
+
+export default App;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./main.scss"
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <p className='nav'> hi akul</p>
+        <div>
+            <div className='vid'>
+                <a href="#"><img src="/akula.gif" alt="" /></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
